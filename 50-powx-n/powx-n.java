533749ae37f22730f162afe5e9718c39d054e92d
SNEHA SATPATHI <snehasatpathi75@gmail.com>
2026-05-12 20:31:59 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (21.76%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public double myPow(double x, int n) {
+
+        long power = n;
+
+        // negative power
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double ans = 1;
+
+        while (power > 0) {
+
+            // if odd
+            if (power % 2 == 1) {
+                ans = ans * x;
+            }
+
+            x = x * x;
+
+            power = power / 2;
+        }
+
+        return ans;
+    }
+}
