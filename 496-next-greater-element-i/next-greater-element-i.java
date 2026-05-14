78fb0b866217240d7103d94505153c06fffde1d8
SNEHA SATPATHI <snehasatpathi75@gmail.com>
2026-05-14 12:59:08 +0530

    Time: 4 ms (75.25%) | Memory: 45.2 MB (49.12%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,24 +1,25 @@
+import java.util.HashMap;
+import java.util.Stack;
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        // Traverse nums2 to find the next greater element for every number
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        
+        // Build the result array for nums1 using the map
         int[] result = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
-            int target = nums1[i];
-            int nextGreater = -1;
-            int foundIndex = -1;
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums2[j] == target) {
-                    foundIndex = j;
-                    break;
-                }
-            }
-            for (int k = foundIndex + 1; k < nums2.length; k++) {
-                if (nums2[k] > target) {
-                    nextGreater = nums2[k];
-                    break;
-                }
-            }
-            result[i] = nextGreater;
+            result[i] = map.getOrDefault(nums1[i], -1);
         }
+        
         return result;
     }
 }
