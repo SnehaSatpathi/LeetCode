343389167155e1b8a1c472a0937c9161c29fb2a3
SNEHA SATPATHI <snehasatpathi75@gmail.com>
2026-05-14 15:35:04 +0530

    Time: 5 ms (48.38%) | Memory: 45 MB (43.05%) - LeetSync

---
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isGood(int[] nums) {
+        int n = nums.length - 1;
+        if (n < 1) return false;
+        Arrays.sort(nums);
+        if (nums[n] != n) {
+            return false;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] != i + 1) {
+                return false;
+            }
+        }
+        return nums[n - 1] == n;
+    }
+}
